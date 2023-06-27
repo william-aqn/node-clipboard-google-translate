da840fe0f3ac25ca256443094a1c2aadc129a1bf
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2023-06-27 16:56:26 +0300

    +deepl

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 
-import notifier from 'node-notifier';
 import { GoogleTranslator } from '@translate-tools/core/translators/GoogleTranslator/index.js';
+import { DeepLTranslator } from '@translate-tools/core/translators/DeepL/index.js';
 import minimist from 'minimist';
+import fs from 'fs';
+
 import {
     FlexLayout,
     QApplication,
-    QClipboard,
     QClipboardMode,
     QCheckBox,
     QLabel,
     QLineEdit,
     CheckState,
     QPlainTextEdit,
-    QScrollArea,
     QComboBox,
     QMainWindow,
     QPushButton,
@@ -21,32 +21,69 @@ import {
 
 class ClipTranslate {
     text: string
-    to: string
-    from: string
-    delay: number
+    configFile: string
+    config: any
     translator: any;
     clipboard: any;
     win: any;
     winStatus: any;
     winText: any;
     winCheckbox: any;
-    clipboardOverride: boolean;
+    winLangFromComboBox: any;
+    winLangToComboBox: any;
 
     constructor() {
+        this.configFile = 'config.json';
+        // Загружаем настройки
+        this.loadConfig();
+        this.text = '';
+        this.clipboard = QApplication.clipboard();
+        // Рисуем окно
+        this.window();
+        // Инициируем переводчик по умолчанию
+        this.initTranslator()
+    }
+
+    loadConfig() {
         var argv = minimist(process.argv.slice(2));
-        this.from = argv.from ?? 'en';
-        this.to = argv.to ?? 'it';
-        this.delay = argv.delay ?? 200;
+        this.config = {};
+        this.config.from = argv.from ?? 'en';
+        this.config.to = argv.to ?? 'zh';
+        this.config.delay = argv.delay ?? 200;
+        this.config.apiKey = argv.key ?? '';
+        this.config.clipboardOverride = false;
+        this.config.translatorCode = argv.translator ?? 'google';
+        try {
+            if (fs.existsSync(this.configFile)) {
+                this.config = JSON.parse(fs.readFileSync(this.configFile, 'utf8'));
+                console.log('config loaded')
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    saveConfig() {
+        try {
+            fs.writeFileSync(this.configFile, JSON.stringify(this.config));
+            console.log('config saved')
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    initGoogleTranslator() {
         this.translator = new GoogleTranslator({
             headers: {
                 'User-Agent':
                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
             },
+            apiKey: this.config.apiKey
         });
-        this.clipboardOverride = true;
-        this.text = '';
-        this.clipboard = QApplication.clipboard();
-        this.window();
+    }
+
+    initDeepL() {
+        this.translator = new DeepLTranslator({ apiKey: this.config.apiKey });
     }
 
     sleep(time: number) {
@@ -63,7 +100,7 @@ class ClipTranslate {
     window() {
         const win = new QMainWindow();
         win.setWindowTitle('Translator');
-        win.resize(200, 200);
+        win.resize(300, 400);
 
         // Основная область
         const centralWidget = new QWidget();
@@ -76,6 +113,11 @@ class ClipTranslate {
         fieldset.setObjectName('operationView');
         const fieldsetLayout = new FlexLayout();
         fieldset.setLayout(fieldsetLayout);
+
+        const translatorView = new QWidget();
+        const translatorLayout = new FlexLayout();
+        translatorView.setObjectName('translatorView');
+        translatorView.setLayout(translatorLayout);
 
         // Кнопка
         const buttonCopy = new QPushButton();
@@ -95,39 +137,45 @@ class ClipTranslate {
         const isRun = new QCheckBox();
         isRun.setText('Включить перевод буфера');
         isRun.setObjectName('isRun');
-        isRun.setCheckState(CheckState.Checked)
+        isRun.setCheckState(this.config.clipboardOverride ? CheckState.Checked : CheckState.Unchecked)
         isRun.addEventListener('toggled', () => {
-            this.clipboardOverride = isRun.isChecked()
-            statusLabel.setText(this.clipboardOverride ? "Перевод буфера активен" : "Отключен перевод буфера");
+            this.config.clipboardOverride = isRun.isChecked()
+            statusLabel.setText(this.config.clipboardOverride ? "Перевод буфера активен" : "Отключен перевод буфера");
+            this.saveConfig();
         });
 
-        // Направление перевода
-        const langFromInput = new QLineEdit();
-        langFromInput.setObjectName('langFromInput');
-        const langToInput = new QLineEdit();
-        langToInput.setObjectName('langToInput');
+        // Api ключ переводчика
+        const apiKeyLabel = new QLabel();
+        apiKeyLabel.setText('API ключ переводчика:');
+        const apiKeyInput = new QLineEdit();
+        apiKeyInput.setObjectName('apiKeyInput');
+        apiKeyInput.setText(this.config.apiKey);
+        apiKeyInput.addEventListener('textChanged', (text) => {
+            this.config.apiKey = text;
+            this.windowStatus(`API ключ установлен ${text}`);
+            this.initTranslator();
+            this.saveConfig();
+        });
 
+        // Выбор переводчика
+        const translatorComboBox = new QComboBox();
+        translatorComboBox.setObjectName('translatorComboBox');
+        translatorComboBox.addItem(undefined, 'google');
+        translatorComboBox.addItem(undefined, 'deepl');
+        translatorComboBox.setCurrentText(this.config.translatorCode);
+
+        translatorComboBox.addEventListener('currentTextChanged', (text) => {
+            this.config.translatorCode = text;
+            this.initTranslator();
+            this.saveConfig();
+        });
+        translatorLayout.addWidget(translatorComboBox);
+
+        // Направление перевода
         const langFromComboBox = new QComboBox();
         langFromComboBox.setObjectName('langFromComboBox');
         const langToComboBox = new QComboBox();
         langToComboBox.setObjectName('langToComboBox');
-
-        // Заполняем направления перевода
-        const availableLangs = GoogleTranslator.getSupportedLanguages()
-        for (let lang of availableLangs) {
-            langFromComboBox.addItem(undefined, lang);
-            langToComboBox.addItem(undefined, lang);
-        }
-        langFromComboBox.addEventListener('currentTextChanged', (text) => {
-            this.from = text;
-            statusLabel.setText(`Перевод с ${this.from}`);
-        });
-        langToComboBox.addEventListener('currentTextChanged', (text) => {
-            this.to = text;
-            statusLabel.setText(`Перевод на ${this.to}`);
-        });
-        langFromComboBox.setCurrentText(this.from)
-        langToComboBox.setCurrentText(this.to)
 
         // Текст с результатом
         const textWork = new QPlainTextEdit();
@@ -136,16 +184,23 @@ class ClipTranslate {
         textWork.setWordWrapMode(3);
         textWork.setPlainText(``);
 
-        // Добавляем кнопку и чекбокс
-        fieldsetLayout.addWidget(buttonCopy);
-        fieldsetLayout.addWidget(isRun);
+        // Добавляем настройки переводчика
+        translatorLayout.addWidget(apiKeyLabel);
+        translatorLayout.addWidget(apiKeyInput);
+
+        // Добавляем направление языка и чекбокс
         fieldsetLayout.addWidget(langFromComboBox);
         fieldsetLayout.addWidget(langToComboBox);
+        fieldsetLayout.addWidget(isRun);
 
-        // Добавляем группу с кнопками и чекбоксом
+        // Добавляем строку с настройками переводчика
+        rootLayout.addWidget(translatorView);
+        // Добавляем строку с кнопками и чекбоксом и направлением перевода
         rootLayout.addWidget(fieldset);
         // Добавляем статус
         rootLayout.addWidget(statusLabel);
+        // Добавляем кнопку скопировать
+        rootLayout.addWidget(buttonCopy);
         // Добавляем поле с результатом
         rootLayout.addWidget(textWork);
 
@@ -157,7 +212,7 @@ class ClipTranslate {
 #operationView{
     flex: 1;
 }
-#buttonCopy, #isRun, #langFromComboBox, #langToComboBox{
+#buttonCopy, #isRun, #langFromComboBox, #langToComboBox, #translatorComboBox{
     flex-direction: row;
 }
 #statusLabel {
@@ -175,37 +230,76 @@ class ClipTranslate {
         this.winStatus = statusLabel;
         this.winText = textWork;
         this.winCheckbox = isRun;
+        this.winLangFromComboBox = langFromComboBox;
+        this.winLangToComboBox = langToComboBox;
+    }
+
+    initTranslator() {
+        if (this.config.translatorCode == 'deepl') {
+            this.initDeepL();
+        } else {
+            this.initGoogleTranslator();
+        }
+        this.windowsInitLangs();
+        this.winStatus?.setText(`Переводчик установлен ${this.config.translatorCode}`);
+    }
+
+    windowsInitLangs() {
+        const obj = (this.translator instanceof DeepLTranslator) ? DeepLTranslator : GoogleTranslator
+        const availableLangs = obj.getSupportedLanguages()
+
+        this.winLangFromComboBox.removeEventListener('currentTextChanged')
+        this.winLangToComboBox.removeEventListener('currentTextChanged')
+        this.winLangFromComboBox.clear();
+        this.winLangToComboBox.clear();
+
+        for (let lang of availableLangs) {
+            this.winLangFromComboBox.addItem(undefined, lang);
+            this.winLangToComboBox.addItem(undefined, lang);
+        }
+
+        this.winLangFromComboBox.addEventListener('currentTextChanged', (text: string) => {
+            this.config.from = text;
+            this.windowStatus(`Перевод с ${this.config.from}`);
+            this.saveConfig();
+        });
+        this.winLangToComboBox.addEventListener('currentTextChanged', (text: string) => {
+            this.config.to = text;
+            this.windowStatus(`Перевод на ${this.config.to}`);
+            this.saveConfig();
+        });
+
+        this.winLangFromComboBox.setCurrentText(this.config.from)
+        this.winLangToComboBox.setCurrentText(this.config.to)
     }
 
     async run() {
-        await this.sleep(this.delay);
+        await this.sleep(this.config.delay);
 
         try {
             let clipText = this.getClipboard()
-            if (clipText != this.text && clipText != '' && this.clipboardOverride) {
-                this.windowStatus('Переводим')
-                this.text = clipText;
-                let translated = await this.translator.translate(this.text, this.from, this.to);
-                notifier.notify({
-                    title: clipText,
-                    message: translated
-                });
+            if (clipText != this.text && clipText != '' && this.config.clipboardOverride) {
+                this.windowStatus('Переводим');
+                this.setText(clipText);
+                let translated = await this.translateText();
                 this.setText(translated);
-                this.windowStatus('Готово')
+                this.windowStatus('Текст в скопирован в буфер обмена');
             }
         } catch (e) {
-            notifier.notify('Error translate');
             this.windowStatus('Ошибка');
             console.log(e);
         }
 
         this.run()
     }
+    async translateText() {
+        return await this.translator.translate(this.text, this.config.from, this.config.to);
+    }
+
     setText(text: string) {
         this.text = text;
         this.winText.setPlainText(this.text);
         this.clipboard.setText(this.text, QClipboardMode.Clipboard);
-        this.windowStatus('Текст в скопирован в буфер обмена');
     }
     getClipboard() {
         return this.clipboard.text(QClipboardMode.Clipboard)
